adf023666b0aff71bcefc81e8f91221661564bf5
Vivianb11 <vivianb.info@gmail.com>
2025-04-12 15:31:06 -0400

    Added automatisation to screen visibility

--- game-jam-behavior-2025/src/components/visibility_component.gd
+++ game-jam-behavior-2025/src/components/visibility_component.gd
@@ -1,13 +1,38 @@
-extends Node3D
+@tool
+extends VisibleOnScreenNotifier3D
 
+signal on_screen_visibility_changed(visible:bool)
 
 @export var points: Array[Node3D]
 
+var state: bool = true :
+	set(value):
+		if value != state:
+			on_screen_visibility_changed.emit(value)
+		state = value
 var camera: Node3D
 
 
 func _ready() -> void:
-	camera = get_viewport().get_camera_3d()
+	if not Engine.is_editor_hint():
+		camera = get_viewport().get_camera_3d()
+
+
+func _physics_process(delta: float) -> void:
+	if Engine.is_editor_hint():
+		for i in range(clamp(points.size(),0,4)):
+			var point_position : Vector3
+			var bb : AABB = self.aabb
+			
+			point_position.z = bb.position.z + bb.size.z/2
+			point_position.x = bb.position.x + bb.size.x if i > 1 else bb.position.x
+			point_position.y = bb.position.y + bb.size.y if i < 1 or i > 2 else bb.position.y
+			
+			points[i].position = point_position
+	else:
+		#Game
+		if self.is_on_screen():
+			state = is_visible_on_screen()
 
 
 func is_visible_on_screen() -> bool:

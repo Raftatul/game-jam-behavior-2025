71e6f825ebfd3a742edb2c3001fc7e89ab042767
Raphaël <daubelcour63@gmail.com>
2025-04-13 10:22:49 -0400

    Add checkpoint animation

--- game-jam-behavior-2025/src/ldos/checkpoint.gd
+++ game-jam-behavior-2025/src/ldos/checkpoint.gd
@@ -1,6 +1,10 @@
 extends Node3D
 
 
+@onready var animation_player: AnimationPlayer = $Checkpoint/AnimationPlayer
+
+
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body is PlayerCharacter:
+		animation_player.play("enter")
 		CheckpointManager.respawn_point = global_position

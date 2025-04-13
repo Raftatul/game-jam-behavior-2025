95479a7a3e698e8971935e65723e0019767cc46a
Raphaël <daubelcour63@gmail.com>
2025-04-13 11:52:59 -0400

    Auto stash before merge of "main" and "origin/main"

--- game-jam-behavior-2025/src/ldos/checkpoint.gd
+++ game-jam-behavior-2025/src/ldos/checkpoint.gd
@@ -1,10 +1,19 @@
 extends Node3D
 
 
+var player: PlayerCharacter
+
 @onready var animation_player: AnimationPlayer = $Checkpoint/AnimationPlayer
 @onready var spawn_point_sound: AudioStreamPlayer = $SpawnPointSound
+@onready var mesh_instance_3d: MeshInstance3D = $MeshInstance3D
 
 var activated:bool = false
+
+
+func _process(delta: float) -> void:
+	if player:
+		mesh_instance_3d.look_at(player.pivot.global_position, Vector3.UP, true)
+
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body is PlayerCharacter and !activated:
@@ -12,3 +21,4 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 		spawn_point_sound.play()
 		CheckpointManager.respawn_point = global_position
 		activated = true
+		player = body

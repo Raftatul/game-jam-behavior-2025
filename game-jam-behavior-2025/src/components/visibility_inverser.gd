4e5dc4a0f1c508430b9bce03fd5fdb7cac01e0ba
Raphaël <daubelcour63@gmail.com>
2025-04-13 14:19:56 -0400

    Quantum mat

---
+++ game-jam-behavior-2025/src/components/visibility_inverser.gd
@@ -0,0 +1,14 @@
+extends MeshInstance3D
+
+
+@export var target: Node3D
+
+
+func _ready() -> void:
+	if target:
+		target.visibility_changed.connect(_on_visibility_changed)
+
+
+func _on_visibility_changed() -> void:
+	if target:
+		visible = target.visible

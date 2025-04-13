38631c35715a9901cc2aaa86d50520e7a84f64c0
Vivianb11 <vivianb.info@gmail.com>
2025-04-13 11:23:23 -0400

    Added Checkpoint sound

--- game-jam-behavior-2025/src/ldos/checkpoint.gd
+++ game-jam-behavior-2025/src/ldos/checkpoint.gd
@@ -2,9 +2,13 @@ extends Node3D
 
 
 @onready var animation_player: AnimationPlayer = $Checkpoint/AnimationPlayer
+@onready var spawn_point_sound: AudioStreamPlayer = $SpawnPointSound
 
+var activated:bool = false
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	if body is PlayerCharacter:
+	if body is PlayerCharacter and !activated:
 		animation_player.play("enter")
+		spawn_point_sound.play()
 		CheckpointManager.respawn_point = global_position
+		activated = true
